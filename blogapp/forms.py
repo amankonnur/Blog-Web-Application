3fb3955b3ba26f7bbfd3cc19a4c71264bb363f9b
amankonnur <amankonnur07@gmail.com>
2025-08-05 19:54:00 +0530

    model and form creation

---
+++ blogapp/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+from blogapp.models import Post
+
+
--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/',null=True,blank=True)
